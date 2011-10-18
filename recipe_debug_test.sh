6682463b8913f5a008ab5ae7238a8da084010e2b
Botykai Zsolt <zsolt.botykai@gmail.com>
2011-10-18 12:58:21 +0200

    - changed the output dirs

--- recipe_debug_test.sh
+++ recipe_debug_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-rm -rf debugdir
+#rm -rf debugdir
 RECIPE_NAME=`basename "${1}" .recipe`
-ebook-convert "${1}" "debugdir/${RECIPE_NAME}/" \
+ebook-convert "${1}" "debugdir/test/${RECIPE_NAME}/" \
  --smarten-punctuation \
  --change-justification justify \
  -vv \
- --debug-pipeline debugdir ${2} \
+ --debug-pipeline debugdir/test/debug \
  | tee debugdir/debug.log

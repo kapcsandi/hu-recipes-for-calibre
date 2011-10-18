babd1d47a9673f742f9c8e3eb475ccf872603753
Botykai Zsolt <zsolt.botykai@gmail.com>
2011-10-18 12:57:45 +0200

    - added RTF output - added possibility to reuse already downloaded HTML sources - extra css usage in PDF output (for proper white BG)

--- recipe_2_mobi_and_pdf.sh
+++ recipe_2_mobi_and_pdf.sh
@@ -3,7 +3,7 @@
 usage()
 {
 cat << EOF
-Usage: $0 -r my.recipe [-m] [-p]
+Usage: $0 -r my.recipe [-m] [-p] [-R] [-t "Title"]
 
 This script converts the supplied recipe to ebook format(s).
 The recipe file should be named like "desired_name.recipe".
@@ -20,14 +20,24 @@ OPTIONS:
    -t      Recipe title (string, e.g. "-t 'Élet és Irodalom'")
    -m      Mobi output will be generated
    -p      PDF output will be generated
+   -R      RTF output will be generated
 EOF
+}
+
+chk_conv_source () {
+    if [[ -s debugdir/${RECIPE_NAME}/processed/index.html ]] ; then
+        export FROMHTM=debugdir/${RECIPE_NAME}/processed/index.html
+    else
+        export FROMHTM=${RECIPE}
+    fi
 }
 
 RECIPE=
 TITLE=
 MOBI=0
 PDF=0
-while getopts “hr:t:mp” OPTION
+RTF=0
+while getopts “hr:t:mpR” OPTION
 do
      case "${OPTION}" in
          h)
@@ -46,6 +56,9 @@ do
          p)
              PDF=1
              ;;
+         R)
+             RTF=1
+             ;;
          ?)
              usage
              exit 1
@@ -56,12 +69,13 @@ done
 [[ -z "${RECIPE}" || ! -f "${RECIPE}" || ! -r "${RECIPE}" || ! -s "${RECIPE}" ]] && \
     { usage ; exit 1 ; }
 
-[[ ${MOBI} -eq 0 && ${PDF} -eq 0 ]] && { MOBI=1 ; PDF=1 ; }   
+[[ ${MOBI} -eq 0 && ${PDF} -eq 0 && ${RTF} -eq 0 ]] && { MOBI=1 ; PDF=1 ; RTF=1 ; }   
 
 [[ -z "${TITLE}" ]] && TITLE="${RECIPE}"
 
 RECIPE_NAME=`basename "${RECIPE}" .recipe`
 
+chk_conv_source && \
 [[ ${MOBI} -eq 1 ]] && \
 ebook-convert "${RECIPE}" "${RECIPE_NAME}.mobi" \
  --output-profile kindle \
@@ -69,22 +83,36 @@ ebook-convert "${RECIPE}" "${RECIPE_NAME}.mobi" \
  --change-justification justify \
  --comments "${TITLE}" \
  --title "${TITLE}" \
- -vv \
- --debug-pipeline debugdir/mobi \
+ -v \
+ --debug-pipeline debugdir/${RECIPE_NAME} \
  | tee debugdir/mobidebug.log
 
+
+chk_conv_source && \
 [[ ${PDF} -eq 1 ]] && \
-ebook-convert "${RECIPE}" "${RECIPE_NAME}.pdf" \
+ebook-convert "${FROMHTM}" "${RECIPE_NAME}.pdf" \
  --smarten-punctuation  \
  --change-justification justify \
+ --extra-css 'body { background-color: white; color: black; }' \
  --paper-size a4  \
  --pretty-print  \
- --preserve-cover-aspect-ratio  \
+ --preserve-cover-aspect-ratio \
  --insert-blank-line  \
  --margin-bottom 72.0  \
  --margin-top 72.0  \
  --margin-left 72.0  \
  --margin-right 72.0  \
- -vv \
- --debug-pipeline debugdir/pdf \
+ -v \
  | tee debugdir/pdfdebug.log
+
+chk_conv_source && \
+[[ ${RTF} -eq 1 ]] && \
+ebook-convert ${FROMHTM} "${RECIPE_NAME}.rtf" \
+     --pretty-print \
+     --smarten-punctuation \
+     --change-justification justify \
+     --extra-css 'body { background-color: white; color: black; }' \
+     --insert-blank-line \
+     -v \
+     | tee debugdir/pdfdebug.log
+
